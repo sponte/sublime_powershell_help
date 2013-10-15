c7e1e8f8a106d2305e0d4093cbafa9ae8ab1e74c
Stanislaw Wozniak <stanislaw.wozniak@easyjet.com>
2013-10-15 15:08:51 +0100

    Adding whatIf and parameters on the cmd line

--- Get-HelpTemplate.ps1
+++ Get-HelpTemplate.ps1
@@ -4,7 +4,8 @@ param(
 
 $c = get-command $commandName
 
-$commonParameters = @("Verbose", "Debug", "ErrorAction", "WarningAction", "ErrorVariable", "WarningVariable", "OutVariable", "OutBuffer")
+$commonParameters = @("Verbose", "Debug", "ErrorAction", "WarningAction", "ErrorVariable", "WarningVariable", "OutVariable", "OutBuffer", "WhatIf")
+$parameters = @()
 
 Write-Output @"
 <#
@@ -16,19 +17,27 @@ Write-Output @"
 
 "@
 
-    $c.Parameters.Keys | %{
-        if($commonParameters -contains $_) {
-            return
-        }
-        $parameter = $c.Parameters[$_]
-        
-        Write-Output @"
+$c.Parameters.Keys | %{
+    if($commonParameters -contains $_) {
+        return
+    }
+    $parameter = $c.Parameters[$_]
+    $parameters += $c.Parameters[$_]
+    
+    Write-Output @"
 .PARAMETER $($parameter.Name)
 
 
 "@
 
-    }
+}
+
+$parametersString = $parameters | %{
+    $name = $_.Name
+    # $value = New-Object ($_.ParameterType.FullName)
+
+    "-$($_.Name) "
+}
 
 Write-Output @"
 .NOTES
@@ -36,7 +45,7 @@ Author: $env:USERNAME
 Created: $([DateTime]::Today.ToString("dd MMMM yyyy"))
 
 .EXAMPLE
-$($c.Name)
+$($c.Name) $parametersString
 
 #>
 "@
